721e7626f80e6cf29e09752a615f0e707a0ea2e8
Babu Thomas <babuthomas138@gmail.com>
2018-03-01 16:26:29 +0530

    Create models for forums app

--- forums/models.py
+++ forums/models.py
@@ -1,3 +1,44 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Board(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
+
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=256)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(
+        Board,
+        on_delete=models.CASCADE,
+        related_name='topics',
+    )
+    starter = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='topics',
+    )
+
+
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(null=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='posts',
+    )
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='+',
+    )
