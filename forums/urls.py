d6511e363154f666e847e0c25f9f3389ee860e74
Babu Thomas <babuthomas138@gmail.com>
2018-03-02 09:41:58 +0530

    Display board breadcrumb

--- forums/urls.py
+++ forums/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BoardListView.as_view(), name='home')
+    path('', views.BoardListView.as_view(), name='home'),
+    path('boards/<int:pk>/', views.BoardDetailView.as_view(), name='board_topics'),
 ]
--- forums/views.py
+++ forums/views.py
@@ -1,8 +1,13 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Board
 
 
 class BoardListView(ListView):
     template_name = 'home.html'
+    model = Board
+
+
+class BoardDetailView(DetailView):
+    template_name = 'topics.html'
     model = Board
