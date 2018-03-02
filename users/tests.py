da1c26799ccb66a876e182c892d6b8179333324f
Babu Thomas <babuthomas138@gmail.com>
2018-03-03 00:57:56 +0530

    Add tests for signup page

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class SignupPageTests(TestCase):
+    def test_signup_page_status_code(self):
+        response = self.client.get('/users/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_uses_correct_template(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'signup.html')
