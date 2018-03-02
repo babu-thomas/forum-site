dd5c5543c40476c8df3b1abf801b8918253b86b2
Babu Thomas <babuthomas138@gmail.com>
2018-03-02 21:42:43 +0530

    Add help text to new topic form

--- forums/forms.py
+++ forums/forms.py
@@ -4,7 +4,11 @@ from .models import Topic
 
 
 class NewTopicForm(forms.ModelForm):
-    message = forms.CharField(widget=forms.Textarea(), max_length=4000)
+    message = forms.CharField(
+        widget=forms.Textarea(),
+        max_length=4000,
+        help_text='Max length of the text is 4000 characters'
+    )
 
     class Meta:
         model = Topic
