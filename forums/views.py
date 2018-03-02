0d0b7852e65365cb066f85ba4927e6bc733ce93a
Babu Thomas <babuthomas138@gmail.com>
2018-03-02 21:32:03 +0530

    Display form for adding new topics

--- forums/views.py
+++ forums/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView
 
-from .models import Board
+from users.models import CustomUser as User
+from .forms import NewTopicForm
+from .models import Board, Post
 
 
 class BoardListView(ListView):
@@ -17,5 +19,25 @@ class BoardDetailView(DetailView):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    context = {'board': board}
+    # TODO: Replace with currently logged in user
+    user = User.objects.first()
+
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user,
+            )
+            # TODO: Redirect to created topic page
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+
+    context = {'board': board, 'form': form}
     return render(request, 'new_topic.html', context)
