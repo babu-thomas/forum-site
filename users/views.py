46a6b3dd0c9de4f85526127f98304aa1786395cd
Babu Thomas <babuthomas138@gmail.com>
2018-03-03 00:48:40 +0530

    Make basic user signup page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from .forms import CustomUserCreationForm
+
+
+class SignupView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'signup.html'
