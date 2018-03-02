7eb447770b87a89cfd9fe0a7c1605f7aa7a0008c
Babu Thomas <babuthomas138@gmail.com>
2018-03-03 01:44:13 +0530

    Switch to function based view for signup page

--- users/views.py
+++ users/views.py
@@ -1,10 +1,18 @@
-from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm as UserCreationForm
 
 
-class SignupView(CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy('home')
-    template_name = 'signup.html'
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form}
+    return render(request, 'signup.html', context)
