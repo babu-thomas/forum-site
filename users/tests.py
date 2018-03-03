75a3bc6ef39187343af983c835bdd3ea2f0ec2be
Babu Thomas <babuthomas138@gmail.com>
2018-03-03 21:10:01 +0530

    Add tests for password reset flow

--- users/tests.py
+++ users/tests.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import PasswordResetForm
+from django.core import mail
 from django.test import TestCase
 from django.urls import reverse
 
@@ -38,6 +40,7 @@ class SignupPageTests(TestCase):
         self.assertIsInstance(form, UserCreationForm)
 
     def test_signup_form_inputs(self):
+        # The form must contain 5 input fields: username, email, password1, password2 and csrf
         response = self.client.get(reverse('signup'))
         self.assertContains(response, '<input', 5)
         self.assertContains(response, 'type="text"', 1)
@@ -67,3 +70,64 @@ class UserCreationFormTests(TestCase):
         expected = ['username', 'email', 'password1', 'password2']
         actual = list(form.fields)
         self.assertSequenceEqual(actual, expected)
+
+
+class PasswordResetTests(TestCase):
+    def setUp(self):
+        self.response = self.client.get(reverse('password_reset'))
+
+    def test_password_reset_page_status_code(self):
+        response = self.client.get('/password_reset/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_password_reset_view_url_by_name(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_password_reset_view_uses_correct_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'password_reset.html')
+
+    def test_password_reset_page_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, PasswordResetForm)
+
+    def test_password_reset_page_form_inputs(self):
+        # The form must contain 2 input fields: email and csrf
+        self.assertContains(self.response, '<input', 2)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, '<button', 1)
+
+
+class SuccessfulPasswordResetTests(TestCase):
+    def setUp(self):
+        username = 'test'
+        email = 'test@example.com'
+        password = 'secretpassword'
+        user_model = get_user_model()
+        user_model.objects.create_user(username=username, email=email, password=password)
+        url = reverse('password_reset')
+        data = {'email': email}
+        self.response = self.client.post(url, data)
+
+    def test_redirection(self):
+        # A valid form submission must redirect user to 'password_reset_done' view
+        url = reverse('password_reset_done')
+        self.assertRedirects(self.response, url)
+
+    def test_send_password_reset_email(self):
+        self.assertEqual(len(mail.outbox), 1)
+
+
+class InvalidPasswordResetTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset')
+        data = {'email': 'invalid@example.com'}
+        self.response = self.client.post(url, data)
+
+    def test_redirection(self):
+        # Even invalid email must redirect user to 'password_reset_done' view
+        url = reverse('password_reset_done')
+        self.assertRedirects(self.response, url)
+
+    def test_no_reset_email_sent(self):
+        self.assertEqual(len(mail.outbox), 0)
