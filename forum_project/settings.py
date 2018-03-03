feb193c1eebbd2e901fcf510e99de92fd815c96c
Babu Thomas <babuthomas138@gmail.com>
2018-03-03 16:15:27 +0530

    Implement logout feature

--- forum_project/settings.py
+++ forum_project/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 AUTH_USER_MODEL = 'users.CustomUser'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGOUT_REDIRECT_URL = 'home'
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
