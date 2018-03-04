d27b603ddbc4354607043e7a8f6f321e3bf612e7
Babu Thomas <babuthomas138@gmail.com>
2018-03-04 19:00:02 +0530

    Add views field to Post model

--- forums/models.py
+++ forums/models.py
@@ -34,6 +34,7 @@ class Topic(models.Model):
         on_delete=models.CASCADE,
         related_name='topics',
     )
+    views = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.subject
