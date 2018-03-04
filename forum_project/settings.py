3178818065ae4b2e48e7943d526f7a88dbe04491
Babu Thomas <babuthomas138@gmail.com>
2018-03-04 22:20:00 +0530

    Configure email backend for sending email through sendgrid

--- forum_project/settings.py
+++ forum_project/settings.py
@@ -133,4 +133,9 @@ LOGOUT_REDIRECT_URL = 'home'
 
 LOGIN_URL = 'login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
